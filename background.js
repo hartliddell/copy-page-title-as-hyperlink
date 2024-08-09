a9be060a1d85610c165ee9d541daa7f5598d0f1e
Hart Liddell <hart@leapingfish.io>
2024-08-08 22:34:11 -0500

    Function name updates

--- background.js
+++ background.js
@@ -1,12 +1,6 @@
-// When the browser action is clicked, `addToClipboard()` will use an offscreen
-// document to write the value of `textToCopy` to the system clipboard.
-chrome.action.onClicked.addListener((tab) => addToClipboard(tab));
+chrome.action.onClicked.addListener(clickHandler);
 
-// Solution 1 - As of Jan 2023, service workers cannot directly interact with
-// the system clipboard using either `navigator.clipboard` or
-// `document.execCommand()`. To work around this, we'll create an offscreen
-// document and pass it the data we want to write to the clipboard.
-async function addToClipboard(tab) {
+async function clickHandler(tab) {
   await chrome.offscreen.createDocument({
     url: "offscreen.html",
     reasons: [chrome.offscreen.Reason.CLIPBOARD],
