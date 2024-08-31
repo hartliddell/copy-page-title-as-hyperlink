9a01e46e5e4873e8dbcd8eef0acb841c6412d8c9
Hart Liddell <hart@leapingfish.io>
2024-08-31 08:45:38 -0500

    Reduce font load

--- content.js
+++ content.js
@@ -32,7 +32,7 @@ async function handler(request) {
 function addGoogleFont() {
   const fontLink = document.createElement("link");
   fontLink.href =
-    "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap";
+    "https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap";
   fontLink.rel = "stylesheet";
   document.head.appendChild(fontLink);
   return fontLink;
